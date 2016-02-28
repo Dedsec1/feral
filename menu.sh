a72f2ff83c3aa92db73df744b1a9099cd4916233
Dedsec1 <sky.net@outlook.com>
2016-02-27 21:52:53 -0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -304,10 +304,10 @@ if [[ "$updatestatus" = "y" ]]
 then
     :
 else
-    echo -e "Hello $(whoami), you have the latest version of the" "\033[36m""$scriptname""\e[0m" "script. This script version is:" "\033[31m""$scriptversion""\e[0m"
-    echo
-    read -ep "The script has been updated, enter [y] to continue or [q] to exit: " -i "y" updatestatus
-    echo
+    #echo -e "Hello $(whoami), you have the latest version of the" "\033[36m""$scriptname""\e[0m" "script. This script version is:" "\033[31m""$scriptversion""\e[0m"
+    #echo
+    #read -ep "The script has been updated, enter [y] to continue or [q] to exit: " -i "y" updatestatus
+    #echo
 fi
 #
 if [[ "$updatestatus" =~ ^[Yy]$ ]]
@@ -326,15 +326,18 @@ do
         "1")
            wget -qO ~/feral-speed.sh https://git.io/v22hr && bash ~/feral-speed.sh
            break
+           wget -qO ~/menu.sh https://git.io/v2as4  && bash ~/menu.sh
             ;;
         "2")
             ##
             wget -qO ~/iocheck.sh https://git.io/v227h && bash ~/iocheck.sh
             break
+            wget -qO ~/menu.sh https://git.io/v2as4  && bash ~/menu.sh
             ;;
         "3")
              wget -qO ~/restart.sh https://git.io/v2afh && bash ~/restart.sh
              break
+             wget -qO ~/menu.sh https://git.io/v2as4  && bash ~/menu.sh
             ;;
         "4")
             host $(hostname -f)
