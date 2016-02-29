24ee6124cd3d64e59b3efdf069b3379dafefcbd4
Dedsec1 <sky.net@outlook.com>
2016-02-28 17:40:24 -0800

    111111

--- install-ratio-plugin.sh
+++ install-ratio-plugin.sh
@@ -6,5 +6,5 @@ cd ~/www/$(whoami).$(hostname -f)/public_html/rutorrent/plugins/
 wget -qO feralstats.zip http://git.io/nB1WyA
 unzip -qo feralstats.zip && rm -f feralstats.zip
 echo  "The Feral Stats Plugin has Sucessfully been installed, Please refresh your Rutorrent page."
-#
+##
 
