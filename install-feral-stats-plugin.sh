70d10e6bd3cf5916d443f2624d5b3ae068460cdb
Dedsec1 <sky.net@outlook.com>
2016-02-28 19:28:23 -0800

    Update install-feral-stats-plugin.sh

--- install-feral-stats-plugin.sh
+++ install-feral-stats-plugin.sh
@@ -6,6 +6,6 @@ cd ~/www/$(whoami).$(hostname -f)/public_html/rutorrent/plugins/
 wget -qO feralstats.zip http://git.io/nB1WyA
 unzip -qo feralstats.zip && rm -f feralstats.zip
 echo  "The Feral Stats Plugin has Sucessfully been installed, Please refresh your Rutorrent page."
-wget -qO ~/test1.sh  https://git.io/v2Vug  && bash ~/test1.sh 
+wget -qO ~/Rutorrent-MainMenu.sh  https://git.io/v2Vug  && bash ~/Rutorrent-MainMenu.sh 
 ##
 
