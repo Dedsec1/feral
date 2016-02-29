98ad55d259ec83ca7d6e02cd9d650f82afda4c48
Dedsec1 <sky.net@outlook.com>
2016-02-28 18:55:00 -0800

    Create install Colored Ratio Column Plugin.sh

---
+++ install Colored Ratio Column Plugin.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+########################
+####Define Variables####
+########################
+wget -qO ~/ratio.zip http://git.io/71cumA
+unzip -qo ~/ratio.zip -d ~/www/$(whoami).$(hostname -f)/public_html/rutorrent/plugins/
+cd && rm -f ratio.zip
+echo  "The Colored Ratio Column Plugin has Sucessfully been installed, Please refresh your Rutorrent page to see the changes."
+##
