9883cbb84d24103dd6835ec9e69d97a048dbeb14
Dedsec1 <sky.net@outlook.com>
2016-02-28 17:21:58 -0800

    Update install-ratio-plugin.sh

--- install-ratio-plugin.sh
+++ install-ratio-plugin.sh
@@ -5,7 +5,6 @@
 cd ~/www/$(whoami).$(hostname -f)/public_html/rutorrent/plugins/
 wget -qO feralstats.zip http://git.io/nB1WyA
 unzip -qo feralstats.zip && rm -f feralstats.zip
-			;;
 	esac
 done
 #
