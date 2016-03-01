3ffec5f3e35cde81c1442fb8c31930c5297eb778
Dedsec1 <sky.net@outlook.com>
2016-02-29 22:02:09 -0800

    Update install-menu.sh

--- install-menu.sh
+++ install-menu.sh
@@ -160,6 +160,7 @@ showMenu ()
     echo "9"": Install Madsonic"
     echo "10"": Install Sickrage"
     echo "11"": Install Subsonic"
+    echo "12"": Quit"
 }
 #
 ###########################
@@ -430,6 +431,10 @@ do
         "11")
             echo "Starting Subsonic Setup "
             wget -qO ~/install.subsonic http://git.io/bGZT && bash ~/install.subsonic
+            break
+            ;;
+        "12")
+            wget -qO ~/menu.sh https://git.io/v2anV && bash ~/menu.sh
             break
             ;;
            
