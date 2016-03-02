94afa016c6b9ae680a9e68959dd15eeff1c65463
Dedsec1 <sky.net@outlook.com>
2016-03-01 16:14:04 -0800

    Delete welcome.sh

--- welcome.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo -e "\033[33m""Welcome $(whoami)""\e[0m"
-screen -ls
-echo -e "\033[33m""You have used $(du -s --si ~/ | awk '{print $1;}')""\e[0m"
-echo -e "\033[33m""Your shared hard drive is $(df  -h $(df -h ~/ | grep dev | awk '{print $1}') | grep dev | awk '{print $5;}') full""\e[0m"
