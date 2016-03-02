767fe479f561b5cbd895355611305e81cc4e0d25
Dedsec1 <sky.net@outlook.com>
2016-03-01 16:35:39 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
-########################
+#################################
 ####Define Variables####
 ########################
 wget -qO ~/bin/quota http://git.io/FolBxw
 mkdir -p ~/bin
 chmod 700 ~/bin/quota
 source ~/.bashrc && source ~/.profile
+#
