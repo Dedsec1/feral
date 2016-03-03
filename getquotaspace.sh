cce89e1ae88c41a7fc136a0b3cd6453f503b9784
Dedsec1 <sky.net@outlook.com>
2016-03-02 21:27:33 -0800

    Update getquotaspace.sh

--- getquotaspace.sh
+++ getquotaspace.sh
@@ -2,7 +2,7 @@
 #################################
 ####Define Variables####
 ########################
-wget -q https://raw.githubusercontent.com/Dedsec1/feral/master/.quotaspace
+wget -qc https://raw.githubusercontent.com/Dedsec1/feral/master/.quotaspace
 mkdir -p ~/bin
 wget -qO ~/bin/quota http://git.io/FolBxw
 chmod 700 ~/bin/quota
