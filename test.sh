5273d1caa2417ffbca1fb5e29813ba7e947ad25a
Dedsec1 <sky.net@outlook.com>
2016-03-04 02:36:59 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
-########################
-####Define Variables####
-########################
+
 mkdir -p ~/spideroak
 wget -qO ~/spideroak.deb 'https://spideroak.com/getbuild?platform=ubuntu&arch=x86_64'
 dpkg-deb -x ~/spideroak.deb ~/spideroak
@@ -13,5 +11,6 @@ sed -i "3iSPIDEROAK_ROOT=$(ls -d $HOME/spideroak)\n" ~/spideroak/SpiderOakONE
 sed -i 's|LD_LIBRARY_PATH="/opt|LD_LIBRARY_PATH="$SPIDEROAK_ROOT/opt|g' ~/spideroak/SpiderOakONE
 sed -i 's|QT_PLUGIN_PATH="/opt|QT_PLUGIN_PATH="$SPIDEROAK_ROOT/opt|g' ~/spideroak/SpiderOakONE
 sed -i 's|exec "/opt|exec "$SPIDEROAK_ROOT/opt|g' ~/spideroak/SpiderOakONE
-#
+break
+;;
 
