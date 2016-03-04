f63d1e8c1fe7aa87fdca2dbe7693f32cc9fbe34f
Dedsec1 <sky.net@outlook.com>
2016-03-04 01:30:22 -0500

    Create dropbox.sh

---
+++ dropbox.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p ~/bin && bash
+wget -qO ~/dropbox.tar.gz "http://www.dropbox.com/download/?plat=lnx.x86_64" && tar -xzf dropbox.tar.gz
+wget -qO ~/bin/dropbox.py "http://www.dropbox.com/download?dl=packages/dropbox.py" && chmod 700 ~/bin/dropbox.py
+source ~/.bashrc && source ~/.profile
+rm -f ~/dropbox.tar.gz
+HOME=$HOME/ ~/.dropbox-dist/dropboxd
+LC_ALL="en_US.UTF-8" LANGUAGE="en_US.UTF-8" HOME=$HOME/ ~/.dropbox-dist/dropboxd
+HOME=$HOME/ ~/.dropbox-dist/dropboxd &
+#
