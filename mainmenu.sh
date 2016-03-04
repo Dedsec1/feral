c8be7808ad99da74ca601641fe39a527f0c8e346
Dedsec1 <sky.net@outlook.com>
2016-03-04 00:21:21 -0500

    Update mainmenu.sh

--- mainmenu.sh
+++ mainmenu.sh
@@ -65,7 +65,7 @@ fi
 ############################
 #
 # Script Version number is set here.
-scriptversion="2.0"
+scriptversion="2.2"
 #
 # Script name goes here. Please prefix with install.
 scriptname="Feral-Toolkit"
@@ -127,7 +127,7 @@ gitissue="https://github.com/feralhosting/feralfilehosting/issues/new"
 ############################
 #
 # Disables the built in script updater permanently by setting this variable to 0.
-updaterenabled="0"
+updaterenabled="1"
 #
 ############################
 ####### Variable End #######
