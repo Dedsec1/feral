1fb25e06cc4d26dc85fdce9320b87f397c32bd3b
Dedsec1 <sky.net@outlook.com>
2016-03-04 02:37:29 -0500

    1321321312313213123123213123213131231111111111111111111111

--- test.sh
+++ test.sh
@@ -11,6 +11,5 @@ sed -i "3iSPIDEROAK_ROOT=$(ls -d $HOME/spideroak)\n" ~/spideroak/SpiderOakONE
 sed -i 's|LD_LIBRARY_PATH="/opt|LD_LIBRARY_PATH="$SPIDEROAK_ROOT/opt|g' ~/spideroak/SpiderOakONE
 sed -i 's|QT_PLUGIN_PATH="/opt|QT_PLUGIN_PATH="$SPIDEROAK_ROOT/opt|g' ~/spideroak/SpiderOakONE
 sed -i 's|exec "/opt|exec "$SPIDEROAK_ROOT/opt|g' ~/spideroak/SpiderOakONE
-break
-;;
+##
 
