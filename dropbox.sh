cb48222d073c732ecac0d2cfb3a341029b9cf5fd
Dedsec1 <sky.net@outlook.com>
2016-03-04 02:18:16 -0500

    Update dropbox.sh

--- dropbox.sh
+++ dropbox.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+#! /bin/bash
+########################
+####Define Variables####
+########################
+mkdir -p ~/bin && bash
 wget -qO ~/dropbox.tar.gz "http://www.dropbox.com/download/?plat=lnx.x86_64" && tar -xzf dropbox.tar.gz
 wget -qO ~/bin/dropbox.py "http://www.dropbox.com/download?dl=packages/dropbox.py" && chmod 700 ~/bin/dropbox.py
 source ~/.bashrc && source ~/.profile
 rm -f ~/dropbox.tar.gz
-break
-#
+########################
+done
