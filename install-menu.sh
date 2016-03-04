e349d758fce34c6d6c2b952ffcfbf48d4a54c11b
Dedsec1 <sky.net@outlook.com>
2016-03-04 01:50:45 -0500

    Update install-menu.sh

--- install-menu.sh
+++ install-menu.sh
@@ -362,9 +362,9 @@ do
             echo "Starting Dropbox Setup"
              wget -O ~/dropbox.py "http://www.dropbox.com/download?dl=packages/dropbox.py"
              chmod 755 ~/dropbox.py
-             sleep 2
              wget -O dropbox.tar.gz "http://www.dropbox.com/download/?plat=lnx.x86_64"
              tar -xvzf dropbox.tar.gz
+             chmod 775 ~/.dropbox-dist/dropboxd
              ~/.dropbox-dist/dropboxd
             break
             ;;
