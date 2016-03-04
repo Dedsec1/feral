59e9f8d609144c0d23d7502fc8303801b8cedca0
Dedsec1 <sky.net@outlook.com>
2016-03-04 02:19:26 -0500

    Update dropbox.sh

--- dropbox.sh
+++ dropbox.sh
@@ -7,6 +7,5 @@ wget -qO ~/dropbox.tar.gz "http://www.dropbox.com/download/?plat=lnx.x86_64" && 
 wget -qO ~/bin/dropbox.py "http://www.dropbox.com/download?dl=packages/dropbox.py" && chmod 700 ~/bin/dropbox.py
 source ~/.bashrc && source ~/.profile
 rm -f ~/dropbox.tar.gz
-########################
-done
+break
 #
