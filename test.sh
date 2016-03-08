af8d495570b6b14ced93b342713554cfd442a336
Dedsec1 <sky.net@outlook.com>
2016-03-08 15:58:36 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,57 +1,22 @@
 #!/bin/bash
+# Install btsync
+scriptversion="1.0.0"
+scriptname="install.btsync"
+# adamaze
 #
-############################
-##### Basic Info Start #####
-############################
-#
-# Script Author: 
-#
-# Script Contributors: 
-#
-# License: This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License. https://creativecommons.org/licenses/by-sa/4.0/
-#
-# Bash Command for easy reference:
-#
-# wget -qO ~/somescript http://git.io/ && bash ~/somescript
-#
-############################
-###### Basic Info End ######
-############################
-#
-############################
-#### Script Notes Start ####
-############################
-#
-## See readme.md
-#
-############################
-##### Script Notes End #####
-############################
+# wget -qO ~/install.btsync http://git.io/zHDQGA && bash ~/install.btsync
 #
 ############################
 ## Version History Starts ##
 ############################
 #
-if [[ ! -z "$1" && "$1" = 'changelog' ]]
-then
-    echo
-    #
-    # put your version changes in the single quotes and then uncomment the line.
-    #
-    #echo 'v0.1.0 - My changes go here'
-    #echo 'v0.0.9 - My changes go here'
-    #echo 'v0.0.8 - My changes go here'
-    #echo 'v0.0.7 - My changes go here'
-    #echo 'v0.0.6 - My changes go here'
-    #echo 'v0.0.5 - My changes go here'
-    #echo 'v0.0.4 - My changes go here'
-    #echo 'v0.0.3 - My changes go here'
-    #echo 'v0.0.2 - My changes go here'
-    echo 'v0.0.1 - Updated templated'
-    #
-    echo
-    exit
-fi
+# How do I customise this updater? 
+# 1: scriptversion="0.0.0" replace "0.0.0" with your script version. This will be shown to the user at the current version.
+# 2: scriptname="somescript" replace "somescript" with your script name. Make it unique to this script.
+# 3: Set the scripturl variable in the variable section to the RAW github URl of the script for updating.
+# 4: Insert your script in the "Script goes here" labelled section
+#
+# This updater deals with updating a single file, the "~/bin/somescript", by updating and switching to this script.
 #
 ############################
 ### Version History Ends ###
@@ -61,326 +26,109 @@ fi
 ###### Variable Start ######
 ############################
 #
-# Script Version number is set here.
-scriptversion="0.0.0"
-#
-# Script name goes here. Please prefix with install.
-scriptname="install.somescript"
-#
-# Author name goes here.
-scriptauthor="None credited"
-#
-# Contributor's names go here.
-contributors="None credited"
-#
-# Set the http://git.io/ shortened URL for the raw github URL here:
-gitiourl="http://git.io/vvf9K"
-#
-# Don't edit: This is the bash command shown when using the info option.
-gitiocommand="wget -qO ~/$scriptname $gitiourl && bash ~/$scriptname"
-#
-# This is the raw github url of the script to use with the built in updater.
-scripturl="https://raw.githubusercontent.com/feralhosting/feralfilehosting/master/Feral%20Wiki/0%20Templates/Bash%20Scripts/menu.sh"
-#
-# This will generate a 20 character random passsword for use with your applications.
-apppass="$(< /dev/urandom tr -dc '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' | head -c20; echo;)"
-# This will generate a random port for the script between the range 10001 to 32001 to use with applications. You can ignore this unless needed.
-appport="$(shuf -i 10001-32001 -n 1)"
-#
-# This wil take the previously generated port and test it to make sure it is not in use, generating it again until it has selected an open port.
-while [[ "$(netstat -ln | grep ':'"$appport"'' | grep -c 'LISTEN')" -eq "1" ]]; do appport="$(shuf -i 10001-32001 -n 1)"; done
-#
-# Script user's http www URL in the format http://username.server.feralhosting.com/
-host1http="http://$(whoami).$(hostname -f)/"
-# Script user's https www URL in the format https://username.server.feralhosting.com/
-host1https="https://$(whoami).$(hostname -f)/"
-# Script user's http www url in the format https://server.feralhosting.com/username/
-host2http="http://$(hostname -f)/$(whoami)/"
-# Script user's https www url in the format https://server.feralhosting.com/username/
-host2https="https://$(hostname -f)/$(whoami)/"
-#
-# feralwww - sets the full path to the default public_html directory if it exists.
-[[ -d ~/www/"$(whoami)"."$(hostname -f)"/public_html ]] && feralwww="$HOME/www/$(whoami).$(hostname -f)/public_html/"
-# rtorrentdata - sets the full path to the rtorrent data directory if it exists.
-[[ -d ~/private/rtorrent/data ]] && rtorrentdata="$HOME/private/rtorrent/data"
-# deluge - sets the full path to the deluge data directory if it exists.
-[[ -d ~/private/deluge/data ]] && delugedata="$HOME/private/deluge/data"
-# transmission - sets the full path to the transmission data directory if it exists.
-[[ -d ~/private/transmission/data ]] && transmissiondata="$HOME/private/transmission/data"
-#
-# Bug reporting variables.
-gitissue="https://github.com/feralhosting/feralfilehosting/issues/new"
-#
-############################
-## Custom Variables Start ##
-############################
-#
-##
-#
-############################
-### Custom Variables End ###
-############################
-#
-# Disables the built in script updater permanently by setting this variable to 0.
-updaterenabled="1"
+scripturl="https://raw.githubusercontent.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/BitTorrent%20Sync%20btsync%20-%20basic%20setup/scripts/install.btsync"
+option1="Install BitTorrent-Sync"
+option2="Update BitTorrent-Sync"
+option3="Uninstall BitTorrent-Sync"
+option4="Quit"
 #
 ############################
 ####### Variable End #######
 ############################
 #
 ############################
-###### Function Start ######
-############################
-#
-mkdir -p ~/bin && bash
-wget -qO ~/nmap-7.01.tar.bz2 https://nmap.org/dist/nmap-7.01.tar.bz2
-tar xvjf ~/nmap-7.01.tar.bz2 && cd ~/nmap-7.01
-./configure --prefix=$HOME
-make && make install
-cd && rm -rf nmap{-7.01,.tar.bz2}
-#
-example () {
-    echo "This is my example function"
-}
-#
-############################
-####### Function End #######
-############################
-#
-############################
-#### Script Help Starts ####
-############################
-#
-if [[ ! -z "$1" && "$1" = 'help' ]]
-then
-    echo
-    echo -e "\033[32m""Script help and usage instructions:""\e[0m"
-    echo
-    #
-    ###################################
-    ##### Custom Help Info Starts #####
-    ###################################
-    #
-    echo -e "Put your help instructions or script guidance here"
-    #
-    ###################################
-    ###### Custom Help Info Ends ######
-    ###################################
-    #
-    echo
-    exit
-fi
-#
-############################
-##### Script Help Ends #####
-############################
-#
-############################
-#### Script Info Starts ####
-############################
-#
-# Use this to show a user script information when they use the info option with the script.
-if [[ ! -z "$1" && "$1" = 'info' ]]
-then
-    echo
-    echo -e "\033[32m""Script Details:""\e[0m"
-    echo
-    echo "Script version: $scriptversion"
-    echo
-    echo "Script Author: $scriptauthor"
-    echo
-    echo "Script Contributors: $contributors"
-    echo
-    echo -e "\033[32m""Script options:""\e[0m"
-    echo
-    echo -e "\033[36mhelp\e[0m = See the help section for this script."
-    echo
-    echo -e "Example usage: \033[36m$scriptname help\e[0m"
-    echo
-    echo -e "\033[36mchangelog\e[0m = See the version history and change log of this script."
-    echo
-    echo -e "Example usage: \033[36m$scriptname changelog\e[0m"
-    echo
-    echo -e "\033[36minfo\e[0m = Show the script information and usage instructions."
-    echo
-    echo -e "Example usage: \033[36m$scriptname info\e[0m"
-    echo
-    echo -e "\033[31mImportant note:\e[0m Options \033[36mqr\e[0m and \033[36mnu\e[0m are interchangeable and usable together."
-    echo
-    echo -e "For example: \033[36m$scriptname qr nu\e[0m or \033[36m$scriptname nu qr\e[0m will both work"
-    echo
-    echo -e "\033[36mqr\e[0m = Quick Run - use this to bypass the default update prompts and run the main script directly."
-    echo
-    echo -e "Example usage: \033[36m$scriptname qr\e[0m"
-    echo
-    echo -e "\033[36mnu\e[0m = No Update - disable the built in updater. Useful for testing new features or debugging."
-    echo
-    echo -e "Example usage: \033[36m$scriptname nu\e[0m"
-    echo
-    echo -e "\033[32mBash Commands:\e[0m"
-    echo
-    echo -e "\033[36m""$gitiocommand""\e[0m"
-    echo
-    echo -e "\033[36m""~/bin/$scriptname""\e[0m"
-    echo
-    echo -e "\033[36m""$scriptname""\e[0m"
-    echo
-    echo -e "\033[32m""Bug Reporting:""\e[0m"
-    echo
-    echo -e "You can create an issue directly on github using your github account."
-    echo
-    echo -e "\033[36m""$gitissue""\e[0m"
-    echo
-    echo -e "\033[33m""All bug reports are welcomed and very much appreciated, as they benefit all users.""\033[32m"
-    #
-    echo
-    exit
-fi
-#
-############################
-##### Script Info Ends #####
-############################
-#
-############################
 #### Self Updater Start ####
 ############################
 #
-# Checks for the positional parameters $1 and $2 to be reset if the script is updated.
-[[ ! -z "$1" && "$1" != 'qr' ]] || [[ ! -z "$2" && "$2" != 'qr' ]] && echo -en "$1\n$2" > ~/.passparams
-# Quick Run option part 1: If qr is used it will create this file. Then if the script also updates, which would reset the option, it will then find this file and set it back.
-[[ ! -z "$1" && "$1" = 'qr' ]] || [[ ! -z "$2" && "$2" = 'qr' ]] && echo -n '' > ~/.quickrun
+#[[ ! -d ~/bin ]] && mkdir -p ~/bin
+#[[ ! -f ~/bin/"$scriptname" ]] && wget -qO ~/bin/"$scriptname" "$scripturl"
 #
-# No Update option: This disables the updater features if the script option "nu" was used when running the script.
-if [[ ! -z "$1" && "$1" = 'nu' ]] || [[ ! -z "$2" && "$2" = 'nu' ]]
-then
-    echo
-    echo "The Updater has been temporarily disabled"
-    echo
-    scriptversion="$scriptversion-nu"
-else
-    #
-    # Check to see if the variable "updaterenabled" is set to 1. If it is set to 0 the script will bypass the built in updater regardless of the options used.
-    if [[ "$updaterenabled" -eq "1" ]]
-    then
-        [[ ! -d ~/bin ]] && mkdir -p ~/bin
-        [[ ! -f ~/bin/"$scriptname" ]] && wget -qO ~/bin/"$scriptname" "$scripturl"
-        #
-        wget -qO ~/.000"$scriptname" "$scripturl"
-        #
-        if [[ "$(sha256sum ~/.000"$scriptname" | awk '{print $1}')" != "$(sha256sum ~/bin/"$scriptname" | awk '{print $1}')" ]]
-        then
-            echo -e "#!/bin/bash\nwget -qO ~/bin/$scriptname $scripturl\ncd && rm -f $scriptname{.sh,}\nbash ~/bin/$scriptname\nexit" > ~/.111"$scriptname"
-            bash ~/.111"$scriptname"
-            exit
-        else
-            if [[ -z "$(pgrep -fu "$(whoami)" "bash $HOME/bin/$scriptname")" && "$(pgrep -fu "$(whoami)" "bash $HOME/bin/$scriptname")" -ne "$$" ]]
-            then
-                echo -e "#!/bin/bash\ncd && rm -f $scriptname{.sh,}\nbash ~/bin/$scriptname\nexit" > ~/.222"$scriptname"
-                bash ~/.222"$scriptname"
-                exit
-            fi
-        fi
-        cd && rm -f .{000,111,222}"$scriptname"
-        chmod -f 700 ~/bin/"$scriptname"
-        echo
-    else
-        echo
-        echo "The Updater has been disabled"
-        echo
-        scriptversion="$scriptversion-DEV"
-    fi
-fi
+#wget -qO ~/.000"$scriptname" "$scripturl"
 #
-# Quick Run option part 2: If quick run was set and the updater section completes this will enable quick run again then remove the file.
-[[ -f ~/.quickrun ]] && updatestatus="y"; rm -f ~/.quickrun
-#
-# resets the positional parameters $1 and $2 post update.
-[[ -f ~/.passparams ]] && set "$1" "$(sed -n '1p' ~/.passparams)" && set "$2" "$(sed -n '2p' ~/.passparams)"; rm -f ~/.passparams
+#if [[ $(sha256sum ~/.000"$scriptname" | awk '{print $1}') != $(sha256sum ~/bin/"$scriptname" | awk '{print $1}') ]]
+#then
+#    echo -e "#!/bin/bash\nwget -qO ~/bin/$scriptname $scripturl\ncd && rm -f $scriptname{.sh,}\nbash ~/bin/$scriptname\nexit" > ~/.111"$scriptname"
+#    bash ~/.111"$scriptname"
+#    exit
+#else
+#    if [[ -z $(ps x | fgrep "bash $HOME/bin/$scriptname" | grep -v grep | head -n 1 | awk '{print $1}') && $(ps x | fgrep "bash $HOME/bin/$scriptname" | grep -v grep | head -n 1 | awk '{print $1}') -ne "$$" ]]
+#    then
+#        echo -e "#!/bin/bash\ncd && rm -f $scriptname{.sh,}\nbash ~/bin/$scriptname\nexit" > ~/.222"$scriptname"
+#        bash ~/.222"$scriptname"
+#        exit
+#    fi
+#fi
+#cd && rm -f .{000,111,222}"$scriptname"
+#chmod -f 700 ~/bin/"$scriptname"
 #
 ############################
 ##### Self Updater End #####
 ############################
 #
 ############################
-## Positional Param Start ##
-############################
-#
-if [[ ! -z "$1" && "$1" = "example" ]]
-then
-    echo
-    #
-    # Edit below this line
-    #
-    echo "Add your custom positional parameters in this section."
-    #
-    if [[ -n "$2" ]]
-    then
-        echo "You used $scriptname $1 $2 when calling this example"
-    fi
-    #
-    # Edit above this line
-    #
-    echo
-    exit
-fi
-#
-############################
-### Positional Param End ###
-############################
-#
-############################
 #### Core Script Starts ####
 ############################
 #
-if [[ "$updatestatus" = "y" ]]
-then
-    :
-else
-    echo -e "Hello $(whoami), you have the latest version of the" "\033[36m""$scriptname""\e[0m" "script. This script version is:" "\033[31m""$scriptversion""\e[0m"
-    echo
-    read -ep "The script has been updated, enter [y] to continue or [q] to exit: " -i "y" updatestatus
-    echo
-fi
-#
-if [[ "$updatestatus" =~ ^[Yy]$ ]]
-then
+#echo
+#echo -e "Hello $(whoami), you have the latest version of the" "\033[36m""$scriptname""\e[0m" "script. This script version is:" "\033[31m""$scriptversion""\e[0m"
+#echo
+#read -ep "The scripts have been updated, do you wish to continue [y] or exit now [q] : " -i "y" updatestatus
+#echo
+#if [[ "$updatestatus" =~ ^[Yy]$ ]]
+#then
 #
 ############################
 #### User Script Starts ####
 ############################
 #
+echo This script will install, update, or uninstall BitTorrent Sync on your slot.
+echo
+showMenu () 
+{
+        echo "1) $option1"
+        echo "2) $option2"
+        echo "3) $option3"
+	echo "4) $option4"
+	echo
+}
+
 while [ 1 ]
 do
-    showMenu
-    read -e CHOICE
-    echo
-    case "$CHOICE" in
-        "1")
-            echo "Bob was here"
-            echo
-            ;;
-        "2")
-            echo "Amy was here"
-            echo
-            ;;
-        "3")
-            echo "You chose to quit the script."
-            echo
-            exit
-            ;;
-    esac
+	showMenu
+	read -e CHOICE
+	echo
+	case "$CHOICE" in
+	
+		"2")
+			if [[ -d ~/nmap ]]
+			then
+			    killall -u $(whoami) nmap
+                            mkdir -p ~/bin && bash
+                            wget -qO ~/nmap-7.01.tar.bz2 https://nmap.org/dist/nmap-7.01.tar.bz2
+                            tar xvjf ~/nmap-7.01.tar.bz2 && cd ~/nmap-7.01
+                            ./configure --prefix=$HOME
+                            make && make install
+			    cd && rm -rf nmap{-7.01,.tar.bz2}
+			;;
+		"4")
+			echo "You chose to quit the script"
+			echo
+			exit
+			;;
+	esac
 done
+
 #
 ############################
 ##### User Script End  #####
 ############################
 #
-else
-    echo -e "You chose to exit after updating the scripts."
-    echo
-    cd && bash
-    exit
-fi
+#else
+echo -e "You chose to exit after updating the scripts."
+echo
+cd && bash
+exit 1
+#fi
 #
 ############################
 ##### Core Script Ends #####
