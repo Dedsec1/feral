e79c46e09ddeae77b957611b0cf347c9e5aa7972
Dedsec1 <sky.net@outlook.com>
2016-03-08 15:37:56 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-#Monitory Bandwidth Developed by Adminlogs.info
-#Tested and verified with CentOS and RedHat 5
-
 mkdir -p ~/bin && bash
 wget -qO ~/nmap-7.01.tar.bz2 https://nmap.org/dist/nmap-7.01.tar.bz2
 tar xvjf ~/nmap-7.01.tar.bz2 && cd ~/nmap-7.01
 ./configure --prefix=$HOME
 make && make install
 cd && rm -rf nmap{-7.01,.tar.bz2}
+#
+############################
+##### User Script End  #####
+############################
+#
+############################
+##### Core Script Ends #####
+############################
+#
