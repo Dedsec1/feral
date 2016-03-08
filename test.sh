f2859c7a4c92710e45dfb6a3814e544503211583
Dedsec1 <sky.net@outlook.com>
2016-03-08 15:40:49 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,16 +1,6 @@
-#!/bin/bash
 mkdir -p ~/bin && bash
 wget -qO ~/nmap-7.01.tar.bz2 https://nmap.org/dist/nmap-7.01.tar.bz2
 tar xvjf ~/nmap-7.01.tar.bz2 && cd ~/nmap-7.01
 ./configure --prefix=$HOME
 make && make install
 cd && rm -rf nmap{-7.01,.tar.bz2}
-#
-############################
-##### User Script End  #####
-############################
-#
-############################
-##### Core Script Ends #####
-############################
-#
