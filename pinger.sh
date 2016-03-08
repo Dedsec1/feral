f0b66d3d81296f4a9af2d3aefb3b466b76a4afa2
Dedsec1 <sky.net@outlook.com>
2016-03-07 23:33:31 -0500

    Create pinger.sh

---
+++ pinger.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+WEBSITE=https://thehawken.org/fs/?s=0
+SUBJECT="$Feral Server might be down!"
+EMAILID="an@email.com"
+STATUS=$(curl -sI $WEBSITE | awk '/HTTP\/1.1/ { print $2 }')
+STRING=$(curl -s $WEBSITE | grep -o "string_to_search")
+VALUE="enceladus
+100%/?/?/?"
+
+if [ $status -ne 100 -a "$STRING" = "$VALUE" ]
+        echo "Website: $WEBSITE is down, status code: '$STATUS' - $(date)" | mail -s "$SUBJECT" $EMAILID
+fi
