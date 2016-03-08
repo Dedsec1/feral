f268ef8ab08a681838bf20e024de9e37cde314fc
Dedsec1 <sky.net@outlook.com>
2016-03-08 18:10:34 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+#
 wget -qO ~/curl.tar.gz http://curl.haxx.se/download/curl-7.41.0.tar.gz
-tar xf ~/curl.tar.gz && cd... ~/curl-7.41.0
-./configure --prefix="$HOME"
+tar xf ~/curl.tar.gz && cd ~/curl-7.41.0
+./configure --prefix="$"HOME
 make && make install
 cd && rm -rf curl{-7.41.0,.tar.gz}
+#
